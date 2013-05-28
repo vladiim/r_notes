a59b2922e8831dba460e8e41bad78ff0513e1345
vladiim <vladiim@yahoo.com.au>
2013-05-28 17:52:05 +1000

    machine_learning/timeseries.R

---
+++ machine_learning/timeseries.R
@@ -0,0 +1,10 @@
+# Autocorrelation v important: acf() 
+
+# To fit autoregressive model: ar()
+
+# To predict take an ar() fit and use predict()
+
+# ts.plot()
+
+# autoregressive integrated moving average ARIMA - arima()
+
